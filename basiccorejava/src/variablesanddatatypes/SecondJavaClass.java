591dea9264e2769f660b57097db61ce96e479e10
shital <wagh.shital@gmail.com>
2019-07-28 11:07:16 +0530

    second java program

---
+++ basiccorejava/src/variablesanddatatypes/SecondJavaClass.java
@@ -0,0 +1,16 @@
+package variablesanddatatypes;
+
+public class SecondJavaClass {
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		//+,-,/,
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a%b);
+	}
+
+}
+
