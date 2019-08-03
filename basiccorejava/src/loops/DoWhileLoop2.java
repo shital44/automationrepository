55df40ae9d16bf7c2ffec02c057975576f591f4e
shital <wagh.shital@gmail.com>
2019-08-03 22:53:54 +0530

    Do while program 2

---
+++ basiccorejava/src/loops/DoWhileLoop2.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class DoWhileLoop2 {
+
+	public static void main(String[] args) {
+		int i=1;
+		do
+		{
+			i++;
+			System.out.println(i);
+		}
+		while(i<10);
+	
+
+	}
+
+}
