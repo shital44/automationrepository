323384aba5e3e91590355eb0a6dfb1e09f4ef610
shital <wagh.shital@gmail.com>
2019-08-03 09:50:27 +0530

    conditional statements

---
+++ basiccorejava/src/conditionalstatements/ConditionalStatements.java
@@ -0,0 +1,53 @@
+package conditionalstatements;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=5;
+		/*if(a>b)
+		{
+			System.out.println("a is greater than b");
+		}
+		
+		else
+		{
+			System.out.println("a is less than b");
+		}*/
+		
+//1	
+		if(a==b)
+	{
+		System.out.println("a is equal to b");
+	}
+	
+	else
+	{
+		System.out.println("a not equal to b");
+	}
+//2
+		/*
+		 * if(a!=b)
+		 *  { 
+		 *  System.out.println("a is not equal to b");
+		 *  }
+		 * 
+		 * else
+		 *  { System.out.println("a is equal to b");
+		 *  }
+		 */
+//3
+		if(a>=b)
+		{
+			System.out.println("a is greater than or equal to b");
+		}
+		
+		else
+		{
+			System.out.println("a is less equal to b");
+		}
+}
+}
+	
+
+   
