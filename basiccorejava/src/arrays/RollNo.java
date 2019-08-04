1adb30180d3a794b44b6abc999b29ed58f32948d
shital <wagh.shital@gmail.com>
2019-08-04 10:42:56 +0530

    Roll no

---
+++ basiccorejava/src/arrays/RollNo.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class RollNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int[] rollno=new int[7];
+     // String[] str=new String[1];
+     // str[0]=" ";
+      rollno[0]=101;
+      rollno[1]=102;
+      rollno[2]=103;
+      rollno[3]=104;
+      rollno[4]=105;
+      rollno[5]=106;
+      rollno[6]=107;
+       
+     // System.out.println(rollno.length);
+	for(int i=0;i<rollno.length;i++) 
+	{
+		//System.out.print(rollno[i]+ str[0]);
+		System.out.print(rollno[i]+"||");
+	}
+	}
+		
+
+}
