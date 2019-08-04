1d42766fb761b8d873eb1367aa668f715c630985
shital <wagh.shital@gmail.com>
2019-08-04 10:40:00 +0530

    one dimentional array

---
+++ basiccorejava/src/arrays/OneDimentionalArray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class OneDimentionalArray {
+	public static void main(String[] args)
+	{
+		String[] str=new String[7];  //array syntax
+		str[0]="Abhishek";
+	    str[1]="Rutuja";
+	    str[2]="Shirin";
+	    str[3]="Shiva";
+	    str[4]="Shital";
+	    str[5]="Sanjeet";
+	    str[6]="Ayushi";
+	   
+	    for(int i=0;i<=6;i++)
+	    {
+	System.out.println(str[i]);
+	}
+}
+}
