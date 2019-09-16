3e531359f1be9c5818b60fdaaa1716b3d4c41686
shital <wagh.shital@gmail.com>
2019-09-16 08:57:47 +0530

    arrays/roll no

--- basiccorejava/src/arrays/RollNo.java
+++ basiccorejava/src/arrays/RollNo.java
@@ -5,8 +5,7 @@ public class RollNo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       int[] rollno=new int[7];
-     // String[] str=new String[1];
-     // str[0]=" ";
+     
       rollno[0]=101;
       rollno[1]=102;
       rollno[2]=103;
